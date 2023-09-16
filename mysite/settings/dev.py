ee471cfc9b26f9a923540017a802289aa53bd4cc
bookchaowalit <bookchaowalit@gmail.com>
2023-09-16 15:59:26 +0700

    first commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-y$ug)#6-+5g+dl2zkmk6j#_^=-9-7cz-bgf6k2=t5jsmd1+6w-"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
